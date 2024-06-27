b4d6bdd8d198723e21aa1a0b01463457d208e0c9
Tyler <Tyler@Keegan.pro>
2024-06-27 16:42:08 -0500

    - Updated minimum deployment to iOS 18 for testing purposes

--- Renfo/App/ContentView.swift
+++ Renfo/App/ContentView.swift
@@ -1,72 +1,72 @@
 import SwiftUI
 
-//@available(iOS 18.0, *)
+@available(iOS 18.0, *)
 struct ContentView: View {
     @AppStorage("appColor") var appColor: AppColor = .default
     @StateObject private var listViewModel = FestivalListViewModel()
 
     var body: some View {
-//        TabView {
-//            // Home Tab
-//            Tab("Festivals", systemImage: "building.columns") {
-//                NavigationStack {
-//                    FestivalListView()
-//                        .environmentObject(listViewModel)
-//                }
-//            }
-//            
-//            // Map Tab
-//            Tab("Map", systemImage: "map") {
-//                NavigationStack {
-//                    MapView()
-//                }
-//            }
-//            
-//            // Settings Tab
-//            Tab("Settings", systemImage: "gearshape.fill") {
-//                NavigationStack {
-//                    SettingsView()
-//                }
-//            }
-//        }
-//        .accentColor(appColor.color)
-//        .tabViewStyle(.sidebarAdaptable)
-        
-        TabView() {
+        TabView {
             // Home Tab
-            NavigationStack {
-                FestivalListView()
-                    .environmentObject(listViewModel)
-            }
-            .tabItem {
-                Image(systemName: "building.columns")
-                Text("Festivals")
+            Tab("Festivals", systemImage: "building.columns") {
+                NavigationStack {
+                    FestivalListView()
+                        .environmentObject(listViewModel)
+                }
             }
             
             // Map Tab
-            NavigationStack {
-                MapView()
+            Tab("Map", systemImage: "map") {
+                NavigationStack {
+                    MapView()
+                }
             }
-            .tabItem {
-                Image(systemName: "map")
-                Text("Map")
-            }
-
+            
             // Settings Tab
-            NavigationStack {
-                SettingsView()
-            }
-            .tabItem {
-                Image(systemName: "gearshape.fill")
-                Text("Settings")
+            Tab("Settings", systemImage: "gearshape.fill") {
+                NavigationStack {
+                    SettingsView()
+                }
             }
         }
         .accentColor(appColor.color)
+        .tabViewStyle(.sidebarAdaptable)
+        
+//        TabView() {
+//            // Home Tab
+//            NavigationStack {
+//                FestivalListView()
+//                    .environmentObject(listViewModel)
+//            }
+//            .tabItem {
+//                Image(systemName: "building.columns")
+//                Text("Festivals")
+//            }
+//            
+//            // Map Tab
+//            NavigationStack {
+//                MapView()
+//            }
+//            .tabItem {
+//                Image(systemName: "map")
+//                Text("Map")
+//            }
+//
+//            // Settings Tab
+//            NavigationStack {
+//                SettingsView()
+//            }
+//            .tabItem {
+//                Image(systemName: "gearshape.fill")
+//                Text("Settings")
+//            }
+//        }
+//        .accentColor(appColor.color)
     }
 }
 
 // MARK: - Preview Provider
-//@available(iOS 18.0, *)
+@available(iOS 18.0, *)
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
